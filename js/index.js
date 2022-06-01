8cabdde498a1e982b9c6378baf317237653b4d05
Luxray555 <100209081+Luxray555@users.noreply.github.com>
2022-06-01 21:03:43 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,108 @@
+//Js animation
+let btnList = document.querySelector('.mobileUserListBtn');
+let listUserDoc = document.querySelector('.listUser');
+btnList.addEventListener('click',() => {
+    if(btnList.classList.contains('open')){
+        listUserDoc.classList.remove('open');
+        btnList.classList.remove('open');
+    }else{
+        listUserDoc.classList.add('open');
+        btnList.classList.add('open');
+    }
+})
+window.addEventListener('resize', () => {
+    if(window.matchMedia("(min-width: 1200px)").matches && listUserDoc.classList.contains('open')){
+        listUserDoc.classList.remove('open');
+        btnList.classList.remove('open');
+    }
+}, true)
+
+
+
+
+//initialise connection server communication
+let socket = io();
+
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+let listMessage = document.getElementById('messages');
+let listLiUser = document.querySelectorAll('.listUser li');
+let listUser;
+
+let createUser = (sck) => {
+    let user = document.createElement('li');
+    user.textContent = sck;
+    listUserDoc.appendChild(user);
+}
+input.addEventListener('focus',() => {
+    socket.emit('focusUser',socket.id);
+})
+input.addEventListener('blur',() => {
+    socket.emit('blurUser',socket.id);
+})
+socket.on('sFocusUser',(sck) => {
+    for(i=0;i<listLiUser.length;i++){
+        if(listLiUser[i].innerHTML == listUser[sck]){
+            listLiUser[i].classList.add('talking');
+        }
+    }
+})
+socket.on('sBlurUser',(sck) => {
+    for(i=0;i<listLiUser.length;i++){
+        if(listLiUser[i].innerHTML == listUser[sck]){
+            listLiUser[i].classList.remove('talking');
+        }
+    }
+})
+
+socket.on('userInteraction',(list) => {
+    listUser = list;
+    listUserDoc.innerHTML="";
+    for(key in listUser){
+        createUser(listUser[key]);
+    }
+    listLiUser = document.querySelectorAll('.listUser li');
+})
+
+socket.on('connect',()=>{
+    socket.on('last messages',(msg) =>{
+        for(i=0;i<msg.length;i++){
+            addMessage(msg[i]['pseudoUser']+' : '+msg[i]['message']);
+        }
+    })
+});
+socket.on("disconnect", () => {
+    for(i=0;i<listUser.length;i++){
+        if(listUser[i]==socket.id){
+            listUserDoc.removeChild(listUserDoc[i]);
+        }
+    }
+});
+
+
+let addMessage = (msg) => {
+    let message = document.createElement('li');
+    message.textContent = msg;
+    listMessage.appendChild(message);
+    listMessage.scrollTo(0, document.body.scrollHeight);
+}
+
+let send = () => {
+    let message = document.getElementById('m').value;
+    socket.emit('chat message', message);
+    console.log('send');
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat message', input.value);
+      input.value = '';
+      input.blur();
+    }
+  });
+
+socket.on('chat message', function(msg) {
+    addMessage(msg);
+});
+
